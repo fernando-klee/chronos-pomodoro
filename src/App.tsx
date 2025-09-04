5f6ff2bbf4fa39ef0a0aad1b2e2018de4a1cfdf0
Fernando K <fernandokochlee@gmail.com>
2025-09-03 21:47:57 -0300

    Creating DefaultInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from './components/Container'
 import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import './styles/global.css'
@@ -18,6 +19,31 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText='task'
+              id='meuIput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   )
