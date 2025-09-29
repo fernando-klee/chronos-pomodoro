28a6abfe106a3be48feccab0911796ac28541989
Fernando K <fernandokochlee@gmail.com>
2025-09-29 00:06:43 -0300

    create taskActions and TaskRducer v1

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -9,50 +9,14 @@ type TaskContextProviderProps = {
 export function TaksContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  type ActionType = {
-    type: string,
-    payload?: number
-  }
 
-  const [myState, dispatch] = useReducer((state, action: ActionType) => {
-    console.log(state, action);
-
-    switch (action.type) {
-      case 'INCREMENT': {
-        if (!action.payload) return state;
-        return {
-          ...state, secondsRemaining: state.secondsRemaining + action.payload,
-        }
-      }
-      case 'DECREMENT': {
-        if (!action.payload) return state;
-        return {
-          ...state, secondsRemaining: state.secondsRemaining - action.payload,
-        }
-      }
-      case 'RESET':
-        return {
-          secondsRemaining: 0
-        }
-    }
-
-    return state;
-  }, {
-    secondsRemaining: 0,
-  })
-
-
-  // useEffect(() => {
-  //   console.log(state);
-  // }, [state])
+  useEffect(() => {
+    console.log(state);
+  }, [state])
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      <h1>O estado é {JSON.stringify(myState)}</h1>
-      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}>Incrementar +10</button>
-      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 20 })}>Incrementar +20</button>
-      <button onClick={() => dispatch({ type: 'DECREMENT', payload: 10 })}>Decrementar -10</button>
-      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+      {children}
     </TaskContext.Provider>
   )
 }
---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,10 @@
+import type { TaskStateModel } from "../../models/TaskStateModel";
+import type { TaskActionModel } from "./taskActions";
+
+export function taskReducer(state: TaskStateModel, action: TaskActionModel): TaskStateModel {
+  switch (action.type) {
+
+  }
+
+  return state;
+}
